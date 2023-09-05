4a373b839ccc6469525fd4ad881bee431ba102a8
Daniel Gazcon <daniel.gazcon27@gmail.com>
2023-09-05 15:46:39 -0400

    testing react ssr

--- customer-ms/pages/index.js
+++ customer-ms/pages/index.js
@@ -1,22 +1,31 @@
+import { DUMMY_DATA } from "@/data/dummy-data";
 import Link from "next/link";
+import fs from 'fs/promises'
+import path from "path";
 
-const HomePage = () => {
+const HomePage = ({products}) => {
+  console.log("component 1");
+
   return (
     <div>
       <div>
         <h1>Customer app </h1>
       </div>
-      <li>
-        <Link href="/customer">Customers</Link>
-      </li>
-      <li>
-        <Link href="/purchase">Puchases</Link>
-      </li>
-      <li>
-        <Link href="/about">About</Link>
-      </li>
+      <ul>
+        {products.map(item => <li key={item.id}>{item.title}</li>)}
+      </ul>
     </div>
   );
 };
+
+export async function getStaticProps() {
+  const pathfile = path.join(process.cwd(), "data", "dummy-data.json")
+  const jsonData = await fs.readFile(pathfile)
+  const data = JSON.parse(jsonData)
+
+  return {props: {
+    products: data
+  }}
+}
 
 export default HomePage;
